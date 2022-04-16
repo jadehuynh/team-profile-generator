66e1377673f7d45defb3be755291036c3ed367e2
jadehuynh <jadehuynh08@gmail.com>
2022-04-16 14:45:31 -0500

    cleaning up code

--- Assets/js/functions.js
+++ Assets/js/functions.js
@@ -1,91 +1,105 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const jest = require('jest');
+const generateHtml = require('.');
 
+const promptQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter your first and last name.',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter your employee ID.',
+        },
+      
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email address.',
+        },
+        {
+            type: 'input',
+            name: 'manager',
+            message: 'Are you a Manager?',
+        },
+        {
+            type: 'input',
+            name: 'engineer',
+            message: 'Are you an Engineer',
+        },
+        {
+            type: 'input',
+            name: 'manager',
+            message: 'Are you an Intern?',
+        },
+    ])
 
-function generateHtml(answers) {
-    return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel="stylesheet" href="./Assets/css/style.css">
-        <title>Team Profile Generator</title>
-    </head>
-    <body>
+    .then(data => {
+       const results = generateHtml(data)
+       fs.writeFileSync("./dest/team.html", results)
+    })
+};
 
-        <nav class="navbar navbar-light bg-primary">
-            <div class="container-fluid">
-              <span class="navbar-brand mb-0 mx-auto fs-1">My Team</span>
-            </div>
-        </nav>
+class managerQuestion extends promptQuestions {
 
-        <div class="container">
-            <div class="card1" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                </div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">An item</li>
-                  <li class="list-group-item">A second item</li>
-                  <li class="list-group-item">A third item</li>
-                </ul>
-              </div>
+    getManagerData(answers) {
+        const managerQuestion =
+    inquirer.prompt([
+    {
+        type: 'input',
+        name: 'office',
+        message: 'Please enter your office number.',
+    },
+])
+    return
 
-              <div class="card2" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                </div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">An item</li>
-                  <li class="list-group-item">A second item</li>
-                  <li class="list-group-item">A third item</li>
-                </ul>
-              </div>
+    }
+}
 
-              <div class="card3" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                </div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">An item</li>
-                  <li class="list-group-item">A second item</li>
-                  <li class="list-group-item">A third item</li>
-                </ul>
-              </div>
+class engineerQuestion extends promptQuestions{
+    
+    getEngineerData () {
+        const engineerQuestion =
+    inquirer.prompt([
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username.',
+    },
+])
+    return
+    }
+}
 
-              <div class="card4" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                </div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">An item</li>
-                  <li class="list-group-item">A second item</li>
-                  <li class="list-group-item">A third item</li>
-                </ul>
-              </div>
+class internQuestion extends promptQuestions{
+    
+    getInternData () {
+        const internQuestion =
+    inquirer.prompt([
+    {
+        type: 'input',
+        name: 'school',
+        message: 'Please enter your school name.',
+    },
+])
+    return
+    }
+}
 
-              <div class="card5" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                </div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">An item</li>
-                  <li class="list-group-item">A second item</li>
-                  <li class="list-group-item">A third item</li>
-                </ul>
-              </div>
-        </div>
-        
-        <script src="./Assets/js/index.js"></script>
-    </body>
-    </html>
-    `
-  }
+const init = () => {
+    console.log(data)
+    
+     fs.writeFileSync("./dest/team.html", generateHtml)
+ }
+
+promptQuestions();
+getManagerData();
+getEngineerData();
+getInternData();
+init();
+
+module.exports = promptQuestions
--- Assets/js/index.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-const jest = require('jest');
-const generateHtml = require('./functions')
-
-const employeeArr = [];
-const newManager = {};
